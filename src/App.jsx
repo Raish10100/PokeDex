b921f2368c51cde2a644ccac45fdeca7e64e6be3
Raish10100 <raishgma101001@gmail.com>
2024-05-17 18:44:08 +0530

    Initial setup complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='title text-center bg-[#cebd2f] px-4 py-2 rounded shadow hover:bg-green-400 focus:outline-none'>PokeDex</h1>
+    </>
+  )
+}
+
+export default App
