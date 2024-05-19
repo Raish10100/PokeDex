7a1ac80641006b41177a48649fdf1655edd33a3c
Raish10100 <raishgma101001@gmail.com>
2024-05-19 18:49:57 +0530

    Pagination added

--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -7,13 +7,20 @@ function PokemonList(){
     const [pokemonList, setPokemonList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    const pokedex_Url = 'https://pokeapi.co/api/v2/pokemon/';
+    const [pokedex_Url, setPokedex_Url] = useState('https://pokeapi.co/api/v2/pokemon/') ;
+
+    const [nextUrl, setNexturl] = useState('')
+    const [prevUrl, setPrevurl] = useState('')
+
 
     async function downloadPokemons(){
         
         const response = await axios.get(pokedex_Url);
         const result = response.data.results;
-        // console.log(result)
+        console.log(response)
+
+        setNexturl(response.data.next);
+        setPrevurl(response.data.previous);
 
         const pokemonResultPromise = result.map(pokemon => axios.get(pokemon.url));
         const pokemonData = await axios.all(pokemonResultPromise);
@@ -36,14 +43,14 @@ function PokemonList(){
 
 useEffect(() => {
             downloadPokemons();
-}, []);
+}, [pokedex_Url]);
 
     return (
         <div className='w-[100%] mx-auto flex flex-col justify-center items-center'>
         <h2 className='text-[35px] mb-[20px] tracking-[5px] text-center'>Pokemon List</h2>
         <div className="btns ">
-            <button className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9] '>Prev</button>
-            <button className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9] '>Next</button>
+            <button className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9] disabled:bg-gray-400' onClick={() => setPokedex_Url(prevUrl)} disabled={prevUrl == null}>Prev</button>
+            <button className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9]  disabled:bg-gray-400' onClick={() => setPokedex_Url(nextUrl)} disabled={nextUrl == null}>Next</button>
         </div>
            <div className='Pokemon-list-wrapper flex flex-wrap justify-center items-center gap-[20px] px-[10px] mt-[35px]'>
            {
@@ -52,8 +59,8 @@ useEffect(() => {
             }
            </div>
            <div className="btns mt-[35px] ">
-                <button className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9] '>Prev</button>
-                <button className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9] '>Next</button>
+                <button className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9] disabled:bg-gray-400 ' onClick={() => setPokedex_Url(prevUrl)} disabled={prevUrl == null}>Prev</button>
+                <button className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9] disabled:bg-gray-400 ' onClick={() => setPokedex_Url(nextUrl)} disabled={nextUrl == null}>Next</button>
             </div>
         </div>
     )
