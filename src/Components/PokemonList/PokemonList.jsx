3c34d38c467ac2307626db5e0413a64b70aab209
Raish10100 <raishgma101001@gmail.com>
2024-05-20 08:56:05 +0530

    Cleaned up the states

--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -2,68 +2,78 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Pokemon from '../Pokemon/Pokemon';
 
-function PokemonList(){
+function PokemonList() {
+    const [pokemonListState, setPokemonListState] = useState({
+        pokemonList: [],
+        isLoading: true,
+        pokedexUrl: 'https://pokeapi.co/api/v2/pokemon/',
+        nextUrl: '',
+        prevUrl: ''
+    });
 
-    const [pokemonList, setPokemonList] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    async function downloadPokemons() {
+        try {
+            const response = await axios.get(pokemonListState.pokedexUrl);
+            const result = response.data.results;
 
-    const [pokedex_Url, setPokedex_Url] = useState('https://pokeapi.co/api/v2/pokemon/') ;
+            const pokemonResultPromise = result.map(pokemon => axios.get(pokemon.url));
+            const pokemonData = await axios.all(pokemonResultPromise);
 
-    const [nextUrl, setNexturl] = useState('')
-    const [prevUrl, setPrevurl] = useState('')
+            const pokeListResult = pokemonData.map((pokeData) => {
+                const pokemon = pokeData.data;
+                return {
+                    id: pokemon.id,
+                    name: pokemon.name,
+                    image: pokemon.sprites.other ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny,
+                    types: pokemon.types
+                };
+            });
 
-
-    async function downloadPokemons(){
-        
-        const response = await axios.get(pokedex_Url);
-        const result = response.data.results;
-        console.log(response)
-
-        setNexturl(response.data.next);
-        setPrevurl(response.data.previous);
-
-        const pokemonResultPromise = result.map(pokemon => axios.get(pokemon.url));
-        const pokemonData = await axios.all(pokemonResultPromise);
-        // console.log(pokemonData)
-
-        const pokeListResult =   pokemonData.map((pokeData) => {
-        const pokemon = pokeData.data;
-            return{
-                id: pokemon.id,
-                name: pokemon.name,
-                image: (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny, 
-                types: pokemon.types
-            }
-        });
-        // console.log(pokeListResult);
-        setPokemonList(pokeListResult);
-        setIsLoading(false)
-       
+            setPokemonListState((state) => ({
+                ...state,
+                pokemonList: pokeListResult,
+                isLoading: false,
+                nextUrl: response.data.next,
+                prevUrl: response.data.previous
+            }));
+        } catch (error) {
+            console.error("Error fetching Pokémon data:", error);
+            setPokemonListState((state) => ({ ...state, isLoading: false }));
+        }
     }
 
-useEffect(() => {
-            downloadPokemons();
-}, [pokedex_Url]);
+    useEffect(() => {
+        downloadPokemons();
+    }, [pokemonListState.pokedexUrl]);
 
     return (
         <div className='w-[100%] mx-auto flex flex-col justify-center items-center'>
-        <h2 className='text-[35px] mb-[20px] tracking-[5px] text-center'>Pokemon List</h2>
-        <div className="btns ">
-            <button className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9] disabled:bg-gray-400' onClick={() => setPokedex_Url(prevUrl)} disabled={prevUrl == null}>Prev</button>
-            <button className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9]  disabled:bg-gray-400' onClick={() => setPokedex_Url(nextUrl)} disabled={nextUrl == null}>Next</button>
-        </div>
-           <div className='Pokemon-list-wrapper flex flex-wrap justify-center items-center gap-[20px] px-[10px] mt-[35px]'>
-           {
-                (isLoading) ? 'Loading...' : 
-                pokemonList.map((p) => <Pokemon  name={p.name} img={p.image} key={p.id} id={p.id}  />)
-            }
-           </div>
-           <div className="btns mt-[35px] ">
-                <button className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9] disabled:bg-gray-400 ' onClick={() => setPokedex_Url(prevUrl)} disabled={prevUrl == null}>Prev</button>
-                <button className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9] disabled:bg-gray-400 ' onClick={() => setPokedex_Url(nextUrl)} disabled={nextUrl == null}>Next</button>
+            <h2 className='text-[35px] mb-[20px] tracking-[5px] text-center'>Pokemon List</h2>
+            <div className="btns">
+                <button
+                    className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9] disabled:bg-gray-400'
+                    onClick={() => setPokemonListState((state) => ({ ...state, pokedexUrl: pokemonListState.prevUrl }))}
+                    disabled={!pokemonListState.prevUrl}
+                >
+                    Prev
+                </button>
+                <button
+                    className='text-black bg-white px-[35px] py-[8px] ml-[15px] rounded-xl hover:bg-[#ffffffb9] disabled:bg-gray-400'
+                    onClick={() => setPokemonListState((state) => ({ ...state, pokedexUrl: pokemonListState.nextUrl }))}
+                    disabled={!pokemonListState.nextUrl}
+                >
+                    Next
+                </button>
+            </div>
+            <div className='Pokemon-list-wrapper flex flex-wrap justify-center items-center gap-[20px] px-[10px] mt-[35px]'>
+                {pokemonListState.isLoading ? 'Loading...' :
+                    pokemonListState.pokemonList.map((p) => (
+                        <Pokemon name={p.name} img={p.image} key={p.id} id={p.id} />
+                    ))
+                }
             </div>
         </div>
-    )
+    );
 }
 
 export default PokemonList;
